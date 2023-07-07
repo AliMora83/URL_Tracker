2f6b12ec72160257c8cacd17cd60a3c0303a381b
Daurius Swarts <ali@openmindi.co.za>
2023-07-07 22:54:35 +0200

    [STYLE] Updated the URL area

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+let myLeads = [];
+const inputEl =
+  document.getElementById("input-el");
+const inputBtn =
+  document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn =
+  document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(
+  localStorage.getItem("myLeads")
+);
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    function (tabs) {
+      myLeads.push(tabs[0].url);
+      localStorage.setItem(
+        "myLeads",
+        JSON.stringify(myLeads)
+      );
+      render(myLeads);
+    }
+  );
+});
 
 function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+  let listItems = "";
+  const maxCharacters = 60; // Specify the maximum number of characters to display
+
+  for (let i = 0; i < leads.length; i++) {
+    const truncatedText =
+      leads[i].length > maxCharacters
+        ? leads[i].slice(0, maxCharacters) + "..."
+        : leads[i];
+    listItems += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                    ${truncatedText}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
+deleteBtn.addEventListener(
+  "dblclick",
+  function () {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+  }
+);
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem(
+    "myLeads",
+    JSON.stringify(myLeads)
+  );
+  render(myLeads);
+});
